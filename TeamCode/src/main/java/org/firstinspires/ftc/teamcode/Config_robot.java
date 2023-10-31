a45e8ff090fbe102719f465ce156a6587e931172
tea-alex <alex1s.fox@yandex.ru>
2023-10-31 08:44:54 -0700

    none

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Config_robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Config_robot.java
@@ -1,14 +1,23 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class Config_robot {
+    private BNO055IMU imu = null;      // Control/Expansion Hub IMU
     public DcMotorEx leftFrontDrive = null;
     public DcMotorEx leftBackDrive = null;
     public DcMotorEx rightFrontDrive = null;
     public DcMotorEx rightBackDrive = null;
+    public DcMotorEx rightLift = null;
+    public DcMotorEx leftLift = null;
+    public DcMotorEx zahvat = null;
+    public Servo servoP = null;
+    public Servo servoS = null;
 
     HardwareMap hardM = null;
 
@@ -18,23 +27,60 @@ public class Config_robot {
 
     public void init_Tele(HardwareMap hMap){
         hardM = hMap;
+
+        servoP = hMap.get(Servo.class,"servoP");
+        servoS = hMap.get(Servo.class, "servoS");
+
         leftFrontDrive  = hardM.get(DcMotorEx.class, "leftFront");
         leftBackDrive  = hardM.get(DcMotorEx.class, "leftRear");
         rightFrontDrive = hardM.get(DcMotorEx.class, "rightFront");
         rightBackDrive = hardM.get(DcMotorEx.class, "rightRear");
+
+        rightLift = hardM.get(DcMotorEx.class, "rightL");
+//        leftLift = hardM.get(DcMotorEx.class, "leftL");
+        zahvat = hardM.get(DcMotorEx.class, "zahvat");
 
         rightFrontDrive.setDirection(DcMotorEx.Direction.FORWARD);
         leftFrontDrive.setDirection(DcMotorEx.Direction.REVERSE);
         rightBackDrive.setDirection(DcMotorEx.Direction.FORWARD);
         leftBackDrive.setDirection(DcMotorEx.Direction.REVERSE);
 
+        zahvat.setDirection(DcMotorSimple.Direction.FORWARD);
+
         rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        rightLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
     public void init_Auto(HardwareMap hMap){
+
         hardM = hMap;
+        servoP = hMap.get(Servo.class,"servo");
+
+        leftFrontDrive  = hardM.get(DcMotorEx.class, "leftFront");
+        leftBackDrive  = hardM.get(DcMotorEx.class, "leftRear");
+        rightFrontDrive = hardM.get(DcMotorEx.class, "rightFront");
+        rightBackDrive = hardM.get(DcMotorEx.class, "rightRear");
+
+        rightLift = hardM.get(DcMotorEx.class, "rightL");
+        leftLift = hardM.get(DcMotorEx.class, "leftL");
+        zahvat = hardM.get(DcMotorEx.class, "zahvat");
+
+        rightFrontDrive.setDirection(DcMotorEx.Direction.FORWARD);
+        leftFrontDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        rightBackDrive.setDirection(DcMotorEx.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotorEx.Direction.REVERSE);
+
+        zahvat.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        rightFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftFrontDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftBackDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        rightLift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Control.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_Control.java
@@ -5,7 +5,6 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 
 @TeleOp(name = "TeleOp")
 @Config
@@ -13,9 +12,17 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 public class TeleOp_Control extends OpMode {
     FtcDashboard dashboard = FtcDashboard.getInstance();
     Config_robot robot = new Config_robot();
+    public OnServo launcher = new OnServo();
+    public static int up_pos = 800;
+    public static int middle_pos = 400;
+    public static int low_pos = 200;
+    public static int zero_pos = 10;
+
     public void init(){
         robot.init_Tele(hardwareMap);
         telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        robot.servoP.setPosition(0.5);
+        robot.servoS.setPosition(0);
     }
 
     public void start(){
@@ -30,10 +37,10 @@ public class TeleOp_Control extends OpMode {
         double lateral =  gamepad1.left_stick_x; // вправо влево
         double yaw     =  gamepad1.right_stick_x; // поворот
 
-        double leftFrontPower  = lateral + yaw; //axial
-        double rightFrontPower = axial - yaw; // - lateral
-        double leftBackPower   = axial + yaw; // - lateral
-        double rightBackPower  = lateral - yaw; // axial
+        double leftFrontPower  = axial + lateral + yaw; //axial
+        double rightFrontPower = axial - lateral - yaw; // - lateral
+        double leftBackPower   = axial - lateral + yaw; // - lateral
+        double rightBackPower  = axial + lateral - yaw; // axial
 
 
         max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
@@ -50,7 +57,89 @@ public class TeleOp_Control extends OpMode {
         robot.rightFrontDrive.setPower(rightFrontPower);
         robot.leftBackDrive.setPower(leftBackPower);
         robot.rightBackDrive.setPower(rightBackPower);
+
+        robot.zahvat.setPower(gamepad1.right_trigger - gamepad1.left_trigger);
+
+        if(gamepad1.y){
+            hight_position();
+        }
+
+        if(gamepad1.b){
+            midle_position();
+        }
+
+        if(gamepad1.a){
+            low_position();
+        }
+
+        if(gamepad1.x){
+            zero_position();
+        }
+
+        if(gamepad1.dpad_left){
+            launcher.launch = true;
+        }
+
+        telemetry.addData("Position rightL: ", robot.rightLift.getCurrentPosition());
+//        telemetry.addData("Position up: ", robot.leftLift.getCurrentPosition());
+        telemetry.update();
+
     }
 
     public void stop(){}
+
+//    void left_lift_regulate(){
+//        int pos_down = robot.leftLift.getCurrentPosition() + 10;
+//        robot.leftLift.setVelocity(500);
+ //       robot.leftLift.setTargetPosition(pos_down);
+//    }
+
+    void right_lift_regulate(){
+        int pos_up = robot.rightLift.getCurrentPosition() + 10;
+        robot.rightLift.setVelocity(500);
+        robot.rightLift.setTargetPosition(pos_up);
+    }
+
+    void hight_position(){
+        robot.rightLift.setVelocity(700);
+        robot.rightLift.setTargetPosition(up_pos);
+    }
+
+    void midle_position(){
+        robot.rightLift.setVelocity(700);
+        robot.rightLift.setTargetPosition(middle_pos);
+    }
+
+    void low_position(){
+        robot.rightLift.setVelocity(700);
+        robot.rightLift.setTargetPosition(low_pos);
+    }
+
+    void zero_position(){
+        robot.rightLift.setVelocity(700);
+        robot.rightLift.setTargetPosition(zero_pos);
+    }
+
+    class OnServo extends  Thread{
+        boolean launch = false;
+        public void run(){
+            while (!isInterrupted()){
+                if(launch){
+                    if (gamepad1.right_bumper) {
+                        robot.servoP.setPosition(0.5);
+                        telemetry.addData(">", "close");
+                        telemetry.update();
+                    }
+                    if (gamepad1.left_bumper){
+                        robot.servoP.setPosition(1);
+                        telemetry.addData(">", "open");
+                        telemetry.update();
+                    }
+                    if(gamepad1.dpad_right){
+                        robot.servoS.setPosition(0.5);
+                    }
+                }
+            }
+        }
+}
 }
